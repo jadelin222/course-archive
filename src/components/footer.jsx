239947daa95283f3f7f8866d0728db8a52425e0b
IsabelFu <147885571+IsabelFu@users.noreply.github.com>
2024-04-18 10:09:07 +0100

    change footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -14,38 +14,43 @@ export default function Footer() {
     <footer>
       {/* First section of the footer */}
       <div>
-        <div className='flex-footer-top'>
-          <div className='footer-flex-item'>
+        <div className="flex-footer-top">
+          <div className="footer-flex-item">
             <h3>Opening Time</h3>
-            <p>[25 April, 2024]</p>
-            <p>6PM - 10PM</p>
-            <p>[26-27 April, 2024]</p>
-            <p>10AM - 8PM</p>
-            <p>[28 April, 2024]</p>
-            <p>10AM - 5PM</p>
+            <div style={{ paddingBottom: '12px' }}>
+              <p>[25 April, 2024]</p>
+              <p>6PM - 10PM</p>
+            </div>
+            <div style={{ paddingBottom: '12px' }}>
+              <p>[26-27 April, 2024]</p>
+              <p>10AM - 8PM</p>
+            </div>
+            <div style={{ paddingBottom: '12px' }}>
+              <p>[28 April, 2024]</p>
+              <p>10AM - 5PM</p>
+            </div>
           </div>
-          <div className='footer-flex-item'>
+          <div className="footer-flex-item">
             <h3>Getting Here</h3>
             <a href="https://www.gold.ac.uk/campus-map/st-james-hatcham-building/#map-view">
-            St. James Hatcham Building (AKA The Church)
-            <Image
-              src="/images/StjamesHatchamresized.jpg"
-              width={150}
-              height={80}
-              alt="Picture of the St. James Hatcham Building"
-            />
+              St. James Hatcham Building (AKA The Church)
+              <Image
+                src="/images/StjamesHatchamresized.jpg"
+                width={150}
+                height={80}
+                alt="Picture of the St. James Hatcham Building"
+              />
             </a>
             <p>- Turn off New Cross road and walk up St James</p>
-            <p>- The St James Hatcham Building is at the end of the road</p>
-            
-           
+            <p style={{ paddingBottom: '12px' }}>- The St James Hatcham Building is at the end of the road</p>
           </div>
         </div>
-        
-       
+
         {/* Display your name and the current year */}
-        <hr></hr>
-        <p>Goldsmiths Digital Arts Computing &copy; {new Date().getFullYear()}</p>
+        <hr style={{ padding: '10px' }}></hr>
+        <p>
+          Goldsmiths Digital Arts Computing &copy; {new Date().getFullYear()}
+        </p>
       </div>
       {/* Second section of the footer */}
       <div>
@@ -53,7 +58,7 @@ export default function Footer() {
         <a href="https://www.instagram.com/digital_arts_computing/?hl=en">
           Find us on Instagram
         </a>
-        <FontAwesomeIcon icon={faInstagram} />
+        <FontAwesomeIcon style={{ 'margin-left': '6px' }} icon={faInstagram} />
         <br></br>
         <a href="https://www.instagram.com/digital_arts_computing/?hl=en">
           Check out our zine
