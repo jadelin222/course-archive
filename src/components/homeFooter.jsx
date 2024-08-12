2c92d14baa8d3985aec2646c50fe601f651e2ed0
yong <109802312+jadelin222@users.noreply.github.com>
2024-08-12 14:45:26 +0100

    typo fix

--- src/components/homeFooter.jsx
+++ src/components/homeFooter.jsx
@@ -14,7 +14,7 @@ export default function HomeFooter() {
       <div>
         <div className="flex-footer-top">
           <div className="footer-flex-item">
-            <h3>Follow us</h3>
+            <h3>Follow Us</h3>
             <div>
              {/* <FontAwesomeIcon style={{ 'paddingRight': '6px' }} icon={faInstagram} /> */}
              <Link href="https://www.instagram.com/digital_arts_computing/?hl=en" target="_blank" rel="noopener noreferrer" className="item">
@@ -36,34 +36,15 @@ export default function HomeFooter() {
             <h3>Join Us</h3>
             <Link href="https://www.gold.ac.uk/ug/bsc-digital-arts-computing/" target="_blank" rel="noopener noreferrer" className="item">Application & Admission via Goldsmiths</Link>
             <Link href="https://www.youtube.com/watch?v=sKPYslKWkGE" target="_blank" rel="noopener noreferrer" className="item">Meet the Students</Link>
-            {/* <p>- Turn off New Cross road and walk up St James</p>
-            <p style={{ paddingBottom: '12px' }}>- The St James Hatcham Building is at the end of the road</p> */}
           </div>
 
           
         </div>
-
-        {/* Display your name and the current year */}
         <hr style={{ padding: '10px' }}></hr>
         <p className="footer-note">Digital Arts Computing form Goldsmiths, University of London</p>
         <p className="footer-note">Website designed and developed by jade lin <a className="underline" href="https://strawberrycongee.itch.io/" target="_blank" rel="noopener noreferrer">@strawberrycongee</a></p>
-        {/* <p>
-          Goldsmiths Digital Arts Computing &copy; {new Date().getFullYear()}
-        </p> */}
       </div>
-      {/* Second section of the footer */}
       <div>
-        {/* provide a link to your social profile */}
-        {/* <FontAwesomeIcon style={{ 'paddingRight': '6px' }} icon={faInstagram} />
-        <Link href="https://www.instagram.com/digital_arts_computing/?hl=en" target="_blank" rel="noopener noreferrer">
-        digital_arts_computing
-        </Link>
-         */}
-        {/* <br></br>
-        <FontAwesomeIcon style={{ 'paddingRight': '6px' }} icon={faBookOpen} />
-        <Link href="https://online.fliphtml5.com/tljyw/cccr/#p=1" target="_blank" rel="noopener noreferrer">
-          Check out our zine
-        </Link> */}
       </div>
     </footer>
   );
